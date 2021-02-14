ba75fc6fc6639af56d3a8843ba09a8d294cae6a8
maifeeulasad <maifeeulasad@gmail.com>
2021-02-14 15:34:25 +0600

    //wip

---
+++ chrome_sfw.js
@@ -0,0 +1,24 @@
+import * as nsfwjs from '/node_modules/nsfwjs/dist/index.js'
+
+(function (){
+	var _images = document.getElementsByTagName("img");
+
+	for(var i = 0; i < _images.length; i++)
+	{		
+		var _img = _images[i];
+		try {
+			if(_img.classList===undefined){
+				_img.setAttribute('class','');
+			}
+			_img.classList.add("chrome-sfw");
+			_img.addEventListener("mouseenter", function() {	
+				_img.classList.remove("chrome-sfw");
+			}, false);
+			_img.addEventListener("mouseleave", function() {
+				_img.classList.add("chrome-sfw");
+			}, false);
+		} catch (e){
+			console.log(e);
+		}
+	}
+})();
